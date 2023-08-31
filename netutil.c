7e697f57a1779665b304f34074cacfd14bffc5ea
Maria-Freitas25 <101955874+Maria-Freitas25@users.noreply.github.com>
2023-08-31 15:08:01 -0400

    Create netutil.c
    Biblioteca

---
+++ netutil.c
@@ -0,0 +1,62 @@
+#include "netutil.h"
+
+struct sockaddr_in * server_address(unsigned short port) {
+
+  struct sockaddr_in * addr;
+
+  addr = malloc(sizeof(struct sockaddr_in));
+
+  if (addr == NULL) {
+    return NULL;
+  }
+
+  memset(addr, 0, sizeof(struct sockaddr_in));
+  addr->sin_family = AF_INET;
+  addr->sin_port = htons(port);
+
+  addr->sin_addr.s_addr = INADDR_ANY;
+
+
+  return addr;
+
+}
+
+struct sockaddr_in * address_by_ip(char * ip, unsigned short port) {
+  struct sockaddr_in * addr;
+
+  addr = malloc(sizeof(struct sockaddr_in));
+
+  if (addr == NULL) {
+    return NULL;
+  }
+
+  memset(addr, 0, sizeof(struct sockaddr_in));
+  addr->sin_family = AF_INET;
+  addr->sin_port = htons(port);
+
+  if (inet_aton(ip, &addr->sin_addr) == 0) {
+    free(addr);
+    return NULL;
+  }
+  return addr;
+}
+
+struct sockaddr_in * address_by_hostname(char * hostname, unsigned short port) {
+  struct hostent* host;
+  struct servent *srv;
+  struct sockaddr_in *addr;
+  addr = malloc(sizeof(struct sockaddr_in));
+
+  if (addr == NULL) {
+    return NULL;
+  }
+
+  host = gethostbyname(hostname); //Obtenha o nome do host (do arquivo /etc/hosts
+  memset(addr, 0, sizeof(addr));
+  addr->sin_family = AF_INET;
+  addr->sin_port = htons(port);
+  addr->sin_addr.s_addr =*(long*)host->h_addr_list[0];
+
+
+  return addr;
+}
